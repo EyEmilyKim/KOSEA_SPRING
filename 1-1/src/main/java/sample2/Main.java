c6df34cf6678667031d9bc4595cd98ad8064c902
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-11-01 10:01:57 +0900

    [UPDATE] 2022.11.01화_ 1-1 복습 : 스프링에서 beans.xml 통한 생성 & 값 주입.

--- 1-1/src/main/java/sample2/Main.java
+++ 1-1/src/main/java/sample2/Main.java
@@ -6,7 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("sample2/beans.xml");
-		MyMessage mm = (MyMessage)ctx.getBean("MyMessageEn");
-		mm.sayHello("My.스프링");
+		MyMessage mmK = (MyMessage)ctx.getBean("MyMessageKr");
+		mmK.sayHello("My.스프링");
+		MyMessage mmE = (MyMessage)ctx.getBean("MyMessageEn");
+		mmE.sayHello("My.스프링");
 	}
 }
--- 1-1/src/main/java/sample3/Main.java
+++ 1-1/src/main/java/sample3/Main.java
@@ -6,11 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("sample3/beans.xml");
-		Human h = (Suzuki)ctx.getBean("suzuki");
+		Human h = (Human)ctx.getBean("suzuki");
 		h.has();
-		Human h2 = (Danaka)ctx.getBean("danaka");
+		Human h2 = (Human)ctx.getBean("danaka");
 		h2.has();
-		Human h3 = (Gobayasi)ctx.getBean("gobayasi");
+		Human h3 = (Human)ctx.getBean("gobayasi");
 		h3.has();
 	}
 }
