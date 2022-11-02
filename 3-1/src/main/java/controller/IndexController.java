6d747ea600f451549c35a6e8bbeac90be60eba4f
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-11-02 10:46:20 +0900

    [UPDATE] 2022.11.01화_  3-1 : 목록 출력 완성

--- 3-1/src/main/java/controller/IndexController.java
+++ 3-1/src/main/java/controller/IndexController.java
@@ -1,17 +1,28 @@
 package controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+import DAO.ItemDAO;
+import model.Item;
 
-	@Override
+public class IndexController implements Controller {
+	private ItemDAO itemDao;
+	public void setItemDao(ItemDAO itemDao) {
+		this.itemDao = itemDao;
+	}
+
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ModelAndView mav = new ModelAndView();
+		List<Item> list = itemDao.findAll();
+		mav.addObject("itemList",list); //DB 조회결과를 ModelAndView에 저장.
+		mav.setViewName("/WEB-INF/jsp/index.jsp"); //JSP 이름을 ModelAndView에 저장.
+		return mav;
 	} //이 메서드는 콜백메서드(자동으로 호출되는 메서드)이다. 
 
 }
